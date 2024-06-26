3b7c068e15e3592a00828c1aa26553093a2dc910
LUIZ HENRIQUE DA CRUZ KIRSCH <0150491>
2024-06-26 19:42:01 -0300

    CRUD de categoria

--- src/main/java/br/com/ienh/springacessobanco/controllers/CategoriaController.java
+++ src/main/java/br/com/ienh/springacessobanco/controllers/CategoriaController.java
@@ -1,16 +1,14 @@
 package br.com.ienh.springacessobanco.controllers;
 
 import br.com.ienh.springacessobanco.dto.CategoriaDTO;
+import br.com.ienh.springacessobanco.dto.ProfessorDTO;
 import br.com.ienh.springacessobanco.services.CategoriaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/categoria")
@@ -33,6 +31,25 @@ public class CategoriaController {
     public String novoSalvar(@Valid @ModelAttribute("categoria") CategoriaDTO categoria, BindingResult bindingResult){
         if(bindingResult.hasErrors()) return "/categoria/novoForm";
         categoriaService.salvarCategoria(categoria);
+        return "redirect:/categoria/listar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable int id){
+        categoriaService.excluixCategoria(id);
+        return "redirect:/categoria/listar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarForm(@PathVariable int id, Model model){
+        model.addAttribute("categoria", categoriaService.obterCategoriaPorID(id));
+        return "/categoria/editarForm";
+    }
+
+    @PostMapping("/editar")
+    public String editarSalvar(@Valid @ModelAttribute("categoria") CategoriaDTO categoria, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) return "/categoria/editarForm";
+        categoriaService.atualizarCategoria(categoria);
         return "redirect:/categoria/listar";
     }
 }
--- src/main/java/br/com/ienh/springacessobanco/services/CategoriaService.java
+++ src/main/java/br/com/ienh/springacessobanco/services/CategoriaService.java
@@ -1,7 +1,9 @@
 package br.com.ienh.springacessobanco.services;
 
 import br.com.ienh.springacessobanco.dto.CategoriaDTO;
+import br.com.ienh.springacessobanco.dto.ProfessorDTO;
 import br.com.ienh.springacessobanco.entities.Categoria;
+import br.com.ienh.springacessobanco.entities.Professor;
 import br.com.ienh.springacessobanco.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,10 +25,28 @@ public class CategoriaService {
         return categorias;
     }
 
+    public void excluixCategoria(int id){
+        categoriaRepository.deleteById(id);
+    }
 
     public void salvarCategoria(CategoriaDTO categoria){
         Categoria novaCategoria = new Categoria();
         novaCategoria.setNome(categoria.nome());
         categoriaRepository.save(novaCategoria);
     }
+
+    public void atualizarCategoria(CategoriaDTO categoria){
+        Categoria atualizarCategoria = new Categoria();
+        atualizarCategoria.setId(categoria.id());
+        atualizarCategoria.setNome(categoria.nome());
+        categoriaRepository.save(atualizarCategoria);
+    }
+
+    public CategoriaDTO obterCategoriaPorID(int id){
+        CategoriaDTO categoriaDTO = null;
+        Categoria categoria = categoriaRepository.findById(id).get();
+        categoriaDTO = new CategoriaDTO(categoria.getId(), categoria.getNome());
+        return categoriaDTO;
+    }
+
 }
