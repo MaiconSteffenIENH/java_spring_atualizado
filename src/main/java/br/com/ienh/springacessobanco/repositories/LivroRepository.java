6147d2f9aee93889b14b3ea2b9f037b6ef22ca7f
MaiconSteffenIENH <maiconsteffen@ienh.com.br>
2024-06-28 10:42:09 -0300

    feat: adicionando CRUD USER

--- src/main/java/br/com/ienh/springacessobanco/repositories/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ienh.springacessobanco.repositories;
-
-import br.com.ienh.springacessobanco.entities.Livro;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LivroRepository extends CrudRepository<Livro, Integer> {
-}
